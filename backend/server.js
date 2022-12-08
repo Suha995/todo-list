acd0d2ed6c6e9a17bdd1d13d1b10f28290f9ac09
Suhair <saljammal995@gmail.com>
2022-12-08 21:54:33 +0200

    setting up express app

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hellllllo");
-});
+// init app & middleware
+const app = express();
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.send({ msg: "welcome to the api" });
 });
